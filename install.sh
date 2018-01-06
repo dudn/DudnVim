4a0e7a3e9efe5a242c8768738e19319fb9dacfd4
Your Name <you@example.com>
2018-01-07 00:55:15 +0800

    vim

---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+#=============================================================================
+# Reset
+Color_off='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'
+Blue='\033[0;34m'
+Green='\033[0;32m'
+
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    msg "${Green}[✔]${Color_off} ${1}${2}"
+}
+
+info() {
+    msg "${Blue}==>${Color_off} ${1}${2}"
+}
+
+error() {
+    msg "${Red}[✘]${Color_off} ${1}${2}"
+    exit 1
+}
+
+need_cmd () {
+    if ! hash "$1" &>/dev/null; then
+        error "Need '$1' (command not fount)"
+	sudo apt install '$1'
+	info "Install '$1' for you"
+    fi
+}
+
+back_up () {
+    if [[ -f "$HOME/.vimrc" ]]; then
+        mv "$HOME/.vimrc" "$HOME/.vimrc_back"
+        success "Backup $HOME/.vimrc to $HOME/.vimrc_back"
+    fi
+
+    if [[ -f "$HOME/.ideavimrc" ]]; then
+        mv "$HOME/.ideavimrc" "$HOME/.ideavimrc_back"
+        success "Backup $HOME/.ideavimrc to $HOME/.ideavimrc_back"
+    fi
+}
+
+check_cmds () {
+    need_cmd 'vim'
+    need_cmd 'curl'
+}
+
+install_plugin_manager () {
+    if [[ ! -f "$HOME/.vim/autoload/plug.vim" ]]; then
+        info "Install plug.vim"
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        success "plug.vim installation done"
+    fi
+}
+
+install_vim () {
+    ln -s "$HOME/.DudnVim/vimrc" "$HOME/.vimrc"
+    ln -s "$HOME/.DudnVim/ideavimrc" "$HOME/.ideavimrc"
+    success "Installed DudnVim"
+}
+
+
+installer () {
+    back_up
+    check_cmds
+    install_package_manager
+    install_vim
+    vim -c PlugInstall
+}
